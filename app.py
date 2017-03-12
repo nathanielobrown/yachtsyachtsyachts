9bb41c2d6debd5747d97aa19757a279ae203dcb0
Nathaniel Brown <nathanielobrown@gmail.com>
2017-03-12 00:25:23 -0500

    de-dupes search results

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 import time
+import pprint
+import urlparse
 
 from celery import Celery
 from flask_cache import Cache
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, g
 
 import scrapers
 
@@ -16,14 +18,20 @@ cache = Cache(app, config={'CACHE_TYPE': 'filesystem',
                            'CACHE_DIR': 'cache'})
 
 
+@app.template_filter('domain_name')
+def domain_name(s):
+    """Takes a URL and returns the domain portion"""
+    return urlparse.urlparse(s).netloc
+
 @celery.task
 def run_search(scraper_name, manufacturer, length):
     scraper = scrapers.all_scrapers[scraper_name]()
     return scraper.search_and_parse(manufacturer, length)
 
 
-@cache.memoize(timeout=600)
+@cache.memoize(timeout=60000)
 def search_all(manufacturer, length):
+    g.cached = False
     async_results = {}
     for scraper_name in scrapers.all_scrapers.iterkeys():
         async_result = run_search.delay(scraper_name, manufacturer, length)
@@ -50,13 +58,27 @@ def home():
 
 @app.route('/search/')
 def search():
+    g.cached = True
     t_start = time.time()
     manufacturer = request.args['manufacturer'].strip()
     length = request.args['length'].strip()
     results = search_all(manufacturer, length)
+    grouped_results = {}
+    for result in results:
+        key = (result['image_hash'], result['year'])
+        grouped_results[key] = grouped_results.get(key, []) + [result]
     elapsed_time = time.time() - t_start
-    return render_template('results.html', results=results,
-                           elapsed_time=elapsed_time)
+    return render_template('results.html', grouped_results=grouped_results,
+                           elapsed_time=elapsed_time, cached=g.cached)
+
+
+
+@app.route('/search_raw/')
+def search_raw():
+    manufacturer = request.args['manufacturer'].strip()
+    length = request.args['length'].strip()
+    results = search_all(manufacturer, length)
+    return '<pre>{}</pre>'.format(pprint.pformat(results))
 
 
 @app.route('/clear_cache/')
